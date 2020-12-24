a45ea1093bed7b183c3b11d2470449e626a9c4f9
Keitaro Shishido <keitaro.shishido@gmail.com>
2020-12-24 14:35:17 +0900

    fixed

--- scripts/get_jws.rb
+++ scripts/get_jws.rb
@@ -14,7 +14,7 @@ payload = {
   # JWT expiration time (10 minute maximum)
   exp: Time.now.to_i + (10 * 60),
   # GitHub App's identifier
-  iss: 13706525
+  iss: 93854
 }
 
 jwt = JWT.encode(payload, private_key, "RS256")
